14b9b50a5e8dfbc4fdd15a7b2c64bec7d469ae9d
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2024-05-14 22:10:43 +0200

    Rename app and add baseUrl.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/guest-book-react/",
   plugins: [react()],
   define: {
     global: "globalThis",
