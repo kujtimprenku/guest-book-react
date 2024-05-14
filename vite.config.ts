62ab42ffb22dde287b879def1b264a8cedad5cff
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2024-05-14 21:32:25 +0200

    Setup context and add polyfills.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    global: "globalThis",
+    "process.env": {},
+  },
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
+});
